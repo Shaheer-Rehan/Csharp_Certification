c20b2793a0f9c29c63b3d82e7a81c780656cf1c4
Shaheer Rehan <y0v95@students.keele.ac.uk>
2025-07-31 10:40:54 +0100

    Learning about Overloaded Methods

---
+++ CsharpProjects/OverloadedMethods/Program.cs
@@ -0,0 +1,7 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(); // this version of the method does not set an upper or lower limit 
+int roll2 = dice.Next(101); // this overloaded version sets an upper limit for the Random number. Notice how if there is only one argument, it defaults as the upper limit
+int roll3 = dice.Next(50, 101); // this version sets a lower and upper limit for the number
+
+Console.WriteLine($"Roll 1: {roll1}\nRoll 2: {roll2}\nRoll 3: {roll3}");
