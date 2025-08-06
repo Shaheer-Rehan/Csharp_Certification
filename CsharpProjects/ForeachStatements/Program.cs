c5e0afa864c7f05534a22fd21de86fc1af761aa0
Shaheer Rehan <y0v95@students.keele.ac.uk>
2025-08-06 17:33:24 +0100

    Leaning about foreach statements

--- CsharpProjects/ForeachStatements/Program.cs
+++ CsharpProjects/ForeachStatements/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿/*Basic use of a foreach statement:
+
+string[] names = ["Ben", "Ridwan", "Shaheer", "Alexa"];
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}*/
+
+int[] inventory = [200, 450, 700, 175, 250];
+int sum = 0;
+int bin = 0;
+foreach (int item in inventory)
+{
+    bin++;
+    sum += item;
+    Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
