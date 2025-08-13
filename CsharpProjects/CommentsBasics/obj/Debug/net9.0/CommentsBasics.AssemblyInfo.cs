a6f36252c4d0d7dbb3821fb3b4e204386966c904
Shaheer Rehan <y0v95@students.keele.ac.uk>
2025-08-13 17:43:12 +0100

    Example of good comments

--- CsharpProjects/CommentsBasics/obj/Debug/net9.0/CommentsBasics.AssemblyInfo.cs
+++ CsharpProjects/CommentsBasics/obj/Debug/net9.0/CommentsBasics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommentsBasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d18884297a6b21ad17fab2e68ced5db0e07565b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4fdaab142703cfcd5798a53ab459f7ad151eee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommentsBasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommentsBasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
