c3bfec7552b886ce9eb42197f0301f0581a60aca
Shaheer Rehan <y0v95@students.keele.ac.uk>
2025-08-14 12:35:40 +0100

    C# Compiler Ignores Whitespace

---
+++ CsharpProjects/Whitespaces/Program.cs
@@ -0,0 +1,12 @@
+﻿// Illustrating how C# compiler ignores whitespaces
+// Example 1: 
+Console
+.
+WriteLine
+(
+"Hello, World!"
+)
+;
+
+// Example 2:  
+string firstWord="Hello";string secondWord="World";Console.WriteLine(firstWord + ", " + secondWord + "!");
