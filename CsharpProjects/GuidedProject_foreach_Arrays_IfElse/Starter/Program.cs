1f067d989759b37eeeb74be4799c57a5b22e61ee
Shaheer Rehan <y0v95@students.keele.ac.uk>
2025-08-18 16:31:15 +0100

    Guided Project - Added extra credit scoring logic

--- CsharpProjects/GuidedProject_foreach_Arrays_IfElse/Starter/Program.cs
+++ CsharpProjects/GuidedProject_foreach_Arrays_IfElse/Starter/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 // initialize variables - graded assignments 
-int currentAssignments = 5;
+int examAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 int[] studentScores = new int[10];
 
@@ -37,14 +37,21 @@ foreach (string name in studentNames)
     int sumAssignmentScores = 0;
     // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade;
+    // initialize/reset a counter for the number of assignments
+    int gradedAssignments = 0;
 
     foreach (int score in studentScores)
     {
-        // Add the exam score to the sum
-        sumAssignmentScores += score;
+        gradedAssignments++;
+
+        if (gradedAssignments <= examAssignments)
+            // Add the exam score to the sum
+            sumAssignmentScores += score;
+        else
+            sumAssignmentScores += score / 10;
     }
 
-    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
