bbecbf49c4647a22575c7f40b324bb79536ec1fc
Shaheer Rehan <y0v95@students.keele.ac.uk>
2025-08-18 16:44:34 +0100

    Guided Project - All requirements satisfied

--- CsharpProjects/GuidedProject_foreach_Arrays_IfElse/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ CsharpProjects/GuidedProject_foreach_Arrays_IfElse/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9921f5f0012fcf50953458792e5e28d53f199088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f067d989759b37eeeb74be4799c57a5b22e61ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
